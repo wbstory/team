7c5294159dbae852c7acc81643be790351e84811
Hyunsik Kim <wbstory@storymate.net>
2016-01-18 11:07:06 +0900

    Added message to work normal.

--- hello.c
+++ hello.c
@@ -4,6 +4,8 @@ int main(int argc, char **argv) {
   int a = 0;
   if(a != 0) {
     printf("This is not possible!\n");
+  } else {
+    printf("Ok. this is normal.\n");
   }
 
   printf("Hello World!\n");
